d3319bb62f13b6a9909f6ed8d3961f1f1e3b4f4f
Oscar Franco <ospfranco@protonmail.com>
2020-12-02 23:12:52 +0100

    bump

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -17,8 +17,8 @@ export async function getCheckRunForAction<E>({
     status: 'in_progress'
   });
 
-  core.info(`ROPO, CHECK RUNS RESPONSE, ${checkRunsResponse}`)
-  core.info(`ROPO, CHECK RUNS RESPONSE, RUNS, ${checkRunsResponse?.data?.check_runs}`)
+  const str = JSON.stringify(checkRunsResponse, null, 4);
+  core.info(`ROPO, CHECK RUNS RESPONSE, ${str}`);
 
   if (checkRunsResponse?.data?.check_runs?.length === 0) {
     throw new Error(`Could not find check run for action: ${name}`);
